8a2debc9cf2bc6ba5fa0383bd60dad6b613e4ecc
Henry Derbyshire <henry@htderbyshire.com>
2022-03-11 17:26:25 +0000

    tidied create grid function

--- app.js
+++ app.js
@@ -15,39 +15,24 @@ function createGrid(gridSize) {
 
   const windowHeight = window.screen.availHeight;
   const windowWidth = window.screen.availWidth;
-  console.log('Total Height: ' + windowHeight);
   const headerHeight = topInfo.offsetHeight;
-  console.log('Header Height: ' + headerHeight);
   gridContainer.style.height = `${
     windowHeight - headerHeight - 1 * rootFontSize[0]
   }px`;
   const gridHeight = gridContainer.offsetHeight;
-  console.log('Grid Height: ' + gridHeight);
   gridContainer.style.width = `${windowWidth - 1 * rootFontSize[0]}px`;
   const gridWidth = gridContainer.offsetWidth;
-  console.log('Grid Width: ' + gridWidth);
 
-  // The width of a single cell -> width of container / number of cells
   const widthOfCell = gridWidth / gridSize;
-  console.log(widthOfCell + 'px');
-
   const columns = gridSize;
   const rows = gridHeight / widthOfCell;
-  // const rows = Math.ceil(gridHeight / widthOfCell);
+
+  console.log('Total Height: ' + windowHeight);
+  console.log('Header Height: ' + headerHeight);
+  console.log('Grid Height: ' + gridHeight);
+  console.log('Grid Width: ' + gridWidth);
   console.log(rows + ' x ' + columns);
-  // const div = gridContainer.getBoundingClientRect();
-  // const gridContainerWidth = div.width;
-  // const gridContainerHeight = div.height;
-  // gridContainer.style.height = `${gridContainerHeight}px`;
-  // console.log('Width: ' + gridContainerWidth);
 
-  // const gridCellDim = gridContainerWidth / gridSize;
-  // const numberOfRows = gridContainerHeight / gridSize;
-  // console.log(gridSize, numberOfRows);
-  // console.log(gridCellDim);
-  // console.log(gridSize * numberOfRows);
-
-  console.log(columns * Math.ceil(rows));
   for (let i = 0; i < columns * Math.ceil(rows); i += 1) {
     const gridCell = document.createElement('div');
     gridCell.classList.add('cell');
@@ -59,34 +44,15 @@ function createGrid(gridSize) {
   gridContainer.style.gridTemplateRows = `repeat(${rows}, ${widthOfCell}px`;
   gridContainer.style.gridTemplateColumns = `repeat(${columns}, ${widthOfCell}px`;
   cellsActiveListener();
-
-  // console.log(gridContainerWidth, gridContainerHeight);
-  // const screenWidth = window.screen.availWidth;
-  // const screenHeight = window.screen.availHeight;
-  // const gridCellDimension = `${screenWidth / gridSize}px`;
-  // console.log('Height: ' + screenHeight);
-  // console.log('Width: ' + screenWidth);
-  // console.log(gridCellDimension);
-  // for (let i = 0; i < gridSize * gridSize; i += 1) {
-  //   const gridCell = document.createElement('div');
-  //   gridCell.classList.add('cell');
-  //   gridCell.style.width = gridCellDimension;
-  //   gridCell.style.height = gridCellDimension;
-  //   gridCell.style.border = '1px solid black';
-  //   gridContainer.appendChild(gridCell);
-  // }
-  // gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${gridCellDimension}`;
-  // gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${gridCellDimension}`;
-  // cellsActiveListener();
 }
+
+createGrid(initialGridSize);
 
 function removeGrid() {
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.lastChild);
   }
 }
-
-createGrid(initialGridSize);
 
 function cellOn() {
   this.style.backgroundColor = 'black';
