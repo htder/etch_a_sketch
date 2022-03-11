4c9333b6d154c90baecdf38841d92eed4886c928
Henry Derbyshire <henry@htderbyshire.com>
2022-03-11 12:50:51 +0000

    added functionality to use a touchscreen to draw on the grid

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function createGrid(gridSize) {
   const gridCellDimension = `${screenWidth / gridSize}px`;
   for (let i = 0; i < gridSize * gridSize; i += 1) {
     const gridCell = document.createElement('div');
+    gridCell.classList.add('cell');
     gridCell.style.width = gridCellDimension;
     gridCell.style.height = gridCellDimension;
     gridCell.style.border = '1px solid black';
@@ -17,6 +18,41 @@ function createGrid(gridSize) {
   }
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${gridCellDimension}`;
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${gridCellDimension}`;
+  cellsActiveListener();
 }
 
 createGrid(initialGridSize);
+
+function cellOn() {
+  this.style.backgroundColor = 'black';
+}
+
+function touchDown() {
+  event.preventDefault();
+  this.style.backgroundColor = 'black';
+}
+
+function touchMove() {
+  event.preventDefault();
+  const target = document.elementFromPoint(
+    event.changedTouches[0].clientX,
+    event.changedTouches[0].clientY
+  );
+  target.style.backgroundColor = 'black';
+}
+
+function cellsActiveListener() {
+  const cells = Array.from(document.querySelectorAll('.cell'));
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', cellOn);
+    cell.addEventListener('touchstart', touchDown);
+    cell.addEventListener('touchmove', touchMove);
+  });
+}
+
+resetButton.addEventListener('click', () => {
+  const cells = Array.from(document.querySelectorAll('.cell'));
+  cells.forEach((cell) => {
+    cell.removeEventListener('mouseenter', cellOn);
+  });
+});
