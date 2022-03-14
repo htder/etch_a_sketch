0e2c85ce862bdd498e35ea15145395f9664f8479
Henry Derbyshire <henry@htderbyshire.com>
2022-03-14 12:32:46 +0000

    reduced large functions into many smaller functions

--- app.js
+++ app.js
@@ -7,39 +7,66 @@ const slider = document.querySelector('.slider');
 const sliderDisplayValue = document.querySelector('.slider-value');
 const root = document.getElementsByTagName('html')[0];
 
-function createGrid(gridSize) {
-  const rootFontSize = window
-    .getComputedStyle(root)
-    .getPropertyValue('font-size')
-    .split('px');
-
-  const windowHeight = document.documentElement.getBoundingClientRect().height;
-  const windowWidth = document.documentElement.getBoundingClientRect().width;
-  if (windowWidth < 400) {
-    slider.setAttribute('max', 30);
-  } else if (windowWidth < 850) {
-    slider.setAttribute('max', 60);
-  }
-  const headerHeight = topInfo.offsetHeight;
-  gridContainer.style.height = `${
-    windowHeight - headerHeight - 1 * rootFontSize[0]
-  }px`;
-  gridContainer.style.width = `${windowWidth - 1 * rootFontSize[0]}px`;
-  const gridHeight = gridContainer.offsetHeight;
-  const gridWidth = gridContainer.offsetWidth;
-
-  const widthOfCell = gridWidth / gridSize;
-  const columns = gridSize;
-  const rows = gridHeight / widthOfCell;
-
+function createCells(container, columns, rows, widthOfCell) {
   for (let i = 0; i < columns * Math.ceil(rows); i += 1) {
     const gridCell = document.createElement('div');
     gridCell.classList.add('cell');
     gridCell.style.width = widthOfCell;
     gridCell.style.height = widthOfCell;
     gridCell.classList.add('colorCell0');
-    gridContainer.appendChild(gridCell);
+    container.appendChild(gridCell);
   }
+}
+
+function setMaxSlider() {
+  const windowWidth = document.documentElement.getBoundingClientRect().width;
+  if (windowWidth < 400) {
+    slider.setAttribute('max', 30);
+  } else if (windowWidth < 850) {
+    slider.setAttribute('max', 60);
+  }
+}
+
+function getPadding(windowWidth) {
+  if (windowWidth > 1300) {
+    return 60;
+  }
+  return 20;
+}
+
+function setGridContainerDimensions() {
+  const rootFontSize = window
+    .getComputedStyle(root)
+    .getPropertyValue('font-size')
+    .split('px');
+  const windowHeight = document.documentElement.getBoundingClientRect().height;
+  const windowWidth = document.documentElement.getBoundingClientRect().width;
+  const headerHeight = topInfo.offsetHeight;
+  const padding = getPadding(windowWidth);
+  if (padding === 60) {
+    gridContainer.style.width = `${1400}px`;
+    gridContainer.style.height = gridContainer.style.width;
+  } else {
+    gridContainer.style.height = `${
+      windowHeight - headerHeight - 1 * rootFontSize[0] - padding
+    }px`;
+    gridContainer.style.width = `${
+      windowWidth - 1 * rootFontSize[0] - padding
+    }px`;
+  }
+}
+
+function createGrid(gridSize) {
+  setMaxSlider();
+  setGridContainerDimensions();
+
+  const gridHeight = gridContainer.offsetHeight;
+  const gridWidth = gridContainer.offsetWidth;
+  const widthOfCell = gridWidth / gridSize;
+  const columns = gridSize;
+  const rows = gridHeight / widthOfCell;
+
+  createCells(gridContainer, columns, rows, widthOfCell);
 
   gridContainer.style.gridTemplateRows = `repeat(${rows}, ${widthOfCell}px`;
   gridContainer.style.gridTemplateColumns = `repeat(${columns}, ${widthOfCell}px`;
@@ -52,8 +79,15 @@ function removeGrid() {
   }
 }
 
-let previousCell;
+function clearGrid() {
+  const cells = Array.from(document.querySelectorAll('.cell'));
+  cells.forEach((cell) => {
+    const color = cell.classList[1];
+    cell.classList.remove(color);
+  });
+}
 
+let previousCell;
 function cellOn() {
   const currentStyleValue = +/\d+/.exec(this.classList[1]);
   if (currentStyleValue === 4) return;
@@ -120,8 +154,5 @@ slider.addEventListener('input', () => {
 });
 
 resetButton.addEventListener('click', () => {
-  const cells = Array.from(document.querySelectorAll('.cell'));
-  removeCellEventListener(cells);
-  removeGrid();
-  createGrid(initialGridSize);
+  clearGrid();
 });
